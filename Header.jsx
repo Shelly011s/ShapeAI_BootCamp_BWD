4dbddc7f758016cb6293a783809da3a453ceaac6
Shelly011s <84381373+Shelly011s@users.noreply.github.com>
2021-06-06 18:37:19 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1> List of My Favorite movies </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> My Favorite movies </h1>
+      <p>
+        My all time favourite movies are Zindagi na milegi dobara,Batman series
+        by Nolan,Up on the poppy Hill,Prisoners,Enemy and many more are yet to
+        be watched.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
